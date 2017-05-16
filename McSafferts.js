57c19a98718881924c15271b268764fa24e46dc3
seanonthelawn <seanonthelawn@gmail.com>
2017-05-16 09:46:28 -0500

    Add files via upload

--- McSafferts.js
+++ McSafferts.js
@@ -27,9 +27,28 @@ $(document).ready(function() {
 		
 		
 	});
+    
+    	$('#button2').click(function(){
+            
+            var pickleBuy1 = $('#dillQty').val();
+            var pickleBuy2 = $('#breadQty').val();
+            var pickleBuy3 = $('#polishQty').val();
+            var pickleBuy4 = $('#gherkinQty').val();
+            
+            PicklePurchase(pickleBuy1,pickleBuy2,pickleBuy3,pickleBuy4)
+
 
 });
+    
+});
 
+    
+function PicklePurchase(pp1, pp2, pp3, pp4) {
+    $('#outputOrderTotal').html('$'+parseFloat((pp1*1.00)+(pp2*1.50)+(pp3*2.00)+(pp4*0.50)).toFixed(2));
+}
+    
+    
+    
 function PickleReccomendation( pp1, pp2) {
 	var pickleReccomend = new Array("Kosher Dill","Bread and Butter","Polish-style Pickle","Gherkin");
 			if (pp1 == "sweet"){
